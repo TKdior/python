8612dacf347a48ce2cc2e8602be7262ae38fc718
TKdior <biheketete@gmail.com>
2025-02-10 13:50:00 +0100

    first commit

---
+++ Day1.py
@@ -0,0 +1,17 @@
+# printing hello world
+print("Hello, World!")
+# Arithmetic operations
+a = 10
+b = 5
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+p = a ** b
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(p)
