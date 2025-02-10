d916976e92f5bede9cfaa4b78a5167c0fb7bc630
TKdior <biheketete@gmail.com>
2025-02-10 15:21:33 +0100

    Operations

--- Day1.py
+++ Day1.py
@@ -15,3 +15,52 @@ print(mul)
 print(div)
 print(mod)
 print(p)
+
+# Boolean Opeartions
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+
+# Comparison Operations
+a = 10
+b = 5
+print(a ==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a>=b)
+print(a<=b)
+print(a is b)
+print(a is not b)
+print(a in [2,5,8,7,4])
+print(b in [7,8,9,6,5])
+print(a not in [1,2,3,4,5,6])
+print(b not in [7,8,9,7,4,1])
+
+# Bitwise Operations
+a = 10
+b = 5
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a<<2)
+print(a>>2)
+print(b>>2)
+print(b<<2)
+
+# Assignment Operations 
+a = 5
+b = a
+print("b =",b)
+b += a
+print("b =",b)
+b -=a
+print("b =",b)
+b *= a
+print("b =",b)
+b <<=a
+print("b =",b)
