f21dd3328fd3c72f39411510ed6f94810d6c224e
TKdior <biheketete@gmail.com>
2025-02-13 14:50:41 +0100

    Basic_bitwise_operations

--- Day5.py
+++ Day5.py
@@ -1,4 +1,4 @@
-def basic_operations():
+def basic_arithmetic_operations():
     print("Select an operation:")
     print("1. Addition")
     print("2. Subtraction")
@@ -34,4 +34,48 @@ def basic_operations():
         print("Invalid choice!")
 
 # Call the function to execute
-basic_operations()
+basic_arithmetic_operations()
+
+
+def basic_bitwise_operations():
+    print("Select an operation:")
+    print("1. AND")
+    print("2. OR")
+    print("3. XOR")
+    print("4. LEFT SHIFT ")
+    print("5. RIGHT SHIFT ")
+    print("6. NOT")
+    print("7. Exit ")
+
+    # Choose a number from 1 to 5
+    choice = int(input("Enter your choice: "))
+    if choice == 7:
+        print("BYE")
+        return
+
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+
+    if choice == 1:
+        result = num1 & num2
+        print(f"AND: {result}")
+    elif choice == 2:
+        result = num1 |  num2
+        print(f"OR: {result}")
+    elif choice == 3:
+        result = num1 ^ num2
+        print(f"XOR: {result}")
+    elif choice == 4:
+        result = num1 << num2
+        print(f"LEFT SHIFT: {result}")
+    elif choice == 5:
+        result = num1 >> num2
+        print(f"RIGHT SHIFT: {result}")
+    elif choice == 6:
+        result = ~num1
+        print(f"NOT: {result}")
+    else:
+        print("Invalid choice!")
+
+# Call the function to execute
+basic_bitwise_operations()
